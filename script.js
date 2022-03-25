1f65a24bc8fb88c6d6364d7c4ef263e58604aa8d
Amarecht <Amarechtato22@gmail.com>
2022-03-25 09:47:15 -0500

    function

---
+++ script.js
@@ -0,0 +1,48 @@
+function sum(a,b){
+    let result;
+    result = a+b;
+    return result
+}
+
+sum(6,8)
+//console.log(sum(6,8))
+sum(9,56)
+//console.log(sum(9,56))
+
+function substract(a,b){
+    let result
+    result = a-b;
+    return result
+}
+substract(12,6)
+console.log(substract(12,6))
+ 
+function multiplication(a,b){
+    let result
+    result = a*b;
+    return result
+}
+
+multiplication(25,5)
+
+console.log(multiplication(25,5))
+
+function division(a,b){
+let result
+result =a/b;
+return result}
+
+division(54,9)
+console.log(division(54,9))
+
+let a,b,c;
+
+a=6;b=8;
+c=a*b
+console.log(c);
+
+function ame(a,b){
+     return a/b
+}
+console.log(ame(20,4))
+
